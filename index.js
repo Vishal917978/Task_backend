603175419d0e32d89412f37b08ad5a18c3f4fdef
Vishal917978 <vishalsharma917978@gmail.com>
2025-09-22 15:49:06 +0530

    updated

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ const app=express();
 const port=3000;        
 const path=require('path');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');                       
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash'); 
+
+
